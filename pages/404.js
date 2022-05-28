ed41c88b7b694c2cf726c69d250435cbbb439d35
adotland <1982042+adotland@users.noreply.github.com>
2022-05-28 03:13:58 -0700

    custom error pages

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import { Box, Heading } from "@chakra-ui/react";
+
+
+export default function Custom404() {
+  return (
+    <Box textAlign={'center'}>
+      <Heading as={'h1'}>404</Heading>
+      <Heading as={'h2'}>Park Not Found</Heading>
+    </Box>
+  )
+}
---
+++ pages/500.js
@@ -0,0 +1,11 @@
+import { Box, Heading } from "@chakra-ui/react";
+
+
+export default function Custom500() {
+  return (
+    <Box textAlign={'center'}>
+      <Heading as={'h1'}>500</Heading>
+      <Heading as={'h2'}>Bike Stolen</Heading>
+    </Box>
+  )
+}
