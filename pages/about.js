cf990f18e64899410c78c19ca58f555c5e536625
adotland <1982042+adotland@users.noreply.github.com>
2022-05-28 02:26:07 -0700

    about page

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,12 @@ import { Box, Text } from "@chakra-ui/react";
 export default function () {
   return (
     <Box my={30} mx={'auto'} maxW={["90%", "90%", "90%", "50%"]}>
-      I started riding my bicycle more often in 2020 as a way to stay healthy and explore the city. Along the way, I got the idea to ride to every park in the Seattle. I did not expect there to be close to four hundred! After completing this challenge, I was down three bikes: one sold, one traded, and one stolen. I was also down 30 lbs and felt much closer to being a Seattlelite. This blog is dedicated to the journey.
+      <Text mb={2}>I got more serious about cycling in 2020 as a way to stay healthy and explore the city, and got the idea to ride to every park in the Seattle. I did not expect there to be close to four hundred!</Text>
+      <Text mb={2}>Along the way, I learned about the P-Patches, so I also rode my bike to every P-Patch.</Text>
+      <Text mb={2}>Along the way, I learned about the Seattle Port Authority owned parks, so I rode my bike to every Port Authority owned park.</Text>
+      <Text mb={2}>Along the way, I learned about the Seattle Public Utilities owned parks, so, can you guess what I did? I rode my bike to every Seattle Public Utilities owned park.</Text>
+      <Text mb={2}>I also covered Mercer Island, and sampled parks from King County, and a few other cities.</Text>
+      <Text mb={2}>After completing this challenge, I was down three bikes: one sold, one traded, and one stolen. I was also down 30 lbs and felt much closer to being a Seattlelite. This blog is dedicated to the journey.</Text>
       <Text fontSize={'xs'} mt={10} pt={5}>This blog is in no way affiliated with any of the organizations representing the places or bicycles pictured</Text>
     </Box>
   )
