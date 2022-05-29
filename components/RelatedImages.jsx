5c4a0b02542d0d3999a470ecf811d18cc7970eee
adotland <1982042+adotland@users.noreply.github.com>
2022-05-29 15:11:17 -0700

    related images

---
+++ components/RelatedImages.jsx
@@ -0,0 +1,33 @@
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function RelatedImages({ dataList }) {
+  return (
+    <Box w={"50%"} my={'auto'} ml={10}>
+      <Heading as={'h3'} fontSize={'large'} mt={5}>Related Images</Heading>
+        {dataList.map((data, index) => (
+          <Link href={`/parks/${data.slug}`} key={index}>
+            <a>
+              <Box
+                minW="20%"
+                m={2}
+                display="inline-block"
+              >
+                <Image
+                  key={index}
+                  src={`https://${process.env.NEXT_PUBLIC_IMG_HOST_DOMAIN}/${data.imageName}.${data.ext}`}
+                  alt={data.name || "image"}
+                  layout="responsive"
+                  width={data.width}
+                  height={data.height}
+                  sizes="15vw"
+                />
+              </Box>
+            </a>
+          </Link>
+        ))
+        }
+    </Box>
+  );
+}
--- pages/parks/[slug].js
+++ pages/parks/[slug].js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { ff } from "fssf";
 import Details from "../../components/Details";
 import SEO from "../../components/SEO/park";
+import RelatedImages from "../../components/RelatedImages";
 
 export async function getStaticPaths() {
   const dataList = await ff.readJson(ff.path(process.cwd(), './cms/data/live/seattle.json'));
@@ -19,32 +20,41 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params: { slug } }) {
   const data = await ff.readJson(ff.path(process.cwd(), './cms/data/live/seattle.json'));
-  return { props: { dataList: data.filter(d => d.slug == slug) } };
+  const currentData = data.filter(d => d.slug == slug).pop();
+  const related = data
+    .filter(d => (d.filters.matchColor === currentData.filters.matchColor && d.slug !== currentData.slug && d.filters.live))
+    .slice(0, 3);
+  return {
+    props: {
+      currentData,
+      related
+    }
+  };
 }
 
-export default function ({ dataList }) {
-  const data = dataList[0];
+export default function ({ currentData, related }) {
   return (
     <>
-      <SEO data={data} />
-      <Flex m={[0, 0, 0, 7]} justifyContent={'space-evenly'} flexDir={['column', 'column', 'column', 'row']} mt={[4,4,4,4]}>
+      <SEO data={currentData} />
+      <Flex m={[0, 0, 0, 7]} justifyContent={'space-evenly'} flexDir={['column', 'column', 'column', 'row']} mt={[4, 4, 4, 4]}>
         <Box flex={1}
-          minW={data.width > data.height ? "60%" : "40%"}
-          maxW={data.width > data.height ? ["100%", "100%", "100%", "65%"] : ["100%", "100%", "100%", "40%"]}
+          minW={currentData.width > currentData.height ? "60%" : "40%"}
+          maxW={currentData.width > currentData.height ? ["100%", "100%", "100%", "65%"] : ["100%", "100%", "100%", "40%"]}
         >
           <Image
-            key={data.id}
-            src={`https://${process.env.NEXT_PUBLIC_IMG_HOST_DOMAIN}/${data.imageName}.${data.ext}`}
-            alt={data.name ?? "image"}
+            key={currentData.id}
+            src={`https://${process.env.NEXT_PUBLIC_IMG_HOST_DOMAIN}/${currentData.imageName}.${currentData.ext}`}
+            alt={currentData.name ?? "image"}
             layout="responsive"
-            width={data.width}
-            height={data.height}
+            width={currentData.width}
+            height={currentData.height}
             // sizes="50vw"
             priority
           />
         </Box>
-        <Details data={data} />
+        <Details data={currentData} />
       </Flex>
+      <RelatedImages dataList={related} />
     </>
   )
 }
