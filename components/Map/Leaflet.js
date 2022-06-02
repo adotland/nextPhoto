d3c55f97098300d0ab6831945c7c672f609bc06f
adotland <1982042+adotland@users.noreply.github.com>
2022-06-02 03:08:31 -0700

    static-ish map on detail page

--- components/Map/Leaflet.js
+++ components/Map/Leaflet.js
@@ -1,10 +1,10 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import styles from './Leaflet.module.css';
 import osm from "../../cms/data/osm-providers";
 
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 
 import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
 import iconUrl from 'leaflet/dist/images/marker-icon.png';
@@ -12,6 +12,8 @@ import shadowUrl from 'leaflet/dist/images/marker-shadow.png';
 import { useColorModeValue } from '@chakra-ui/react';
 
 const Leaflet = ({ center, name }) => {
+
+  const ZOOM = 11;
 
   useEffect(() => {
     (async function init() {
@@ -27,8 +29,27 @@ const Leaflet = ({ center, name }) => {
 
   const tileProvider = useColorModeValue('jawgLight', 'jawgDark');
 
+  const interactionOptions = {
+    zoomControl: false,
+    doubleClickZoom: false,
+    closePopupOnClick: false,
+    dragging: true,
+    zoomSnap: true,
+    zoomDelta: true,
+    trackResize: true,
+    touchZoom: false,
+    scrollWheelZoom: false,
+  };
+
+  function ChangeView({ center, }) {
+    const map = useMap();
+    map.setView(center, ZOOM);
+    return null;
+  }
+
   return (
-    <MapContainer className={styles.map} center={center} zoom={13} scrollWheelZoom={false}>
+    <MapContainer className={styles.map} center={center} zoom={ZOOM} {...interactionOptions}>
+      <ChangeView center={center} />
       <TileLayer
         url={osm[tileProvider].url}
         attribution={osm[tileProvider].attribution}
