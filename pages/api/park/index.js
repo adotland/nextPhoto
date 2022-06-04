1a30f20f655ac6f3ef4b8ad7934e14013daf46ca
adotland <1982042+adotland@users.noreply.github.com>
2022-06-04 12:13:16 -0700

    move api data

--- pages/api/park/index.js
+++ pages/api/park/index.js
@@ -1,19 +1,21 @@
 import Fuse from 'fuse.js'
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-const dataList = require("../../../cms/data/live/data/api_data.json");
+// import { createRequire } from "module";
+// const require = createRequire(import.meta.url);
+// const dataList = require("../../../cms/data/live/data/api_data.json");
+
+import { dataList } from './api_data'
 
 export default async function handler(req, res) {
   let { query } = (req.query)
-  query = query.substring(0,10).replaceAll(/[^a-z\s]/ig,'')?.toLowerCase();
+  query = query.substring(0, 10).replaceAll(/[^a-z\s]/ig, '')?.toLowerCase();
 
   const options = {
     includeScore: false,
     keys: ['parkName']
   }
-  
+
   const fuse = new Fuse(dataList, options)
-  
+
   const retval = fuse.search(query)
 
   return res.status(200).json({ props: { searchDataList: retval.slice(0, 5) } });
