28897e3d77bcfe98775474c0af643bde126f40b6
adotland <1982042+adotland@users.noreply.github.com>
2022-06-07 16:08:43 -0700

    csp

--- pages/_document.js
+++ pages/_document.js
@@ -3,20 +3,18 @@
 import { ColorModeScript, } from '@chakra-ui/react'
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
 import theme from '../utils/theme'
-import crypto from 'crypto'
+// import crypto from 'crypto'
 
-const cspHashOf = (text) => {
-  const hash = crypto.createHash('sha256')
-  hash.update(text)
-  return `'sha256-${hash.digest('base64')}'`
-}
+// const cspHashOf = (text) => {
+//   const hash = crypto.createHash('sha256')
+//   hash.update(text)
+//   return `'sha256-${hash.digest('base64')}'`
+// }
 
 export default class Document extends NextDocument {
 
   render() {
-    let csp = `default-src 'self'; img-src https://*.jawg.io 'self' data:; style-src 'self' 'unsafe-inline'; script-src 'self' ${cspHashOf(
-      NextScript.getInlineScriptSource(this.props)
-    )}`
+    let csp = `default-src 'self'; img-src https://*.jawg.io 'self' data:; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline'; connect-src: https://vitals.vercel-insights.com`
     if (process.env.NODE_ENV !== 'production') {
       csp = `default-src 'self'; img-src https://*.jawg.io 'self' data:; style-src 'self' 'unsafe-inline'; script-src 'unsafe-inline' 'unsafe-eval' 'self' `
     }
