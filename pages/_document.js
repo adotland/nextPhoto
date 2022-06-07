1147045a308696e92913b757c786de133af5fd5d
adotland <1982042+adotland@users.noreply.github.com>
2022-06-07 15:43:45 -0700

    csp

--- pages/_document.js
+++ pages/_document.js
@@ -3,12 +3,30 @@
 import { ColorModeScript, } from '@chakra-ui/react'
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
 import theme from '../utils/theme'
+import crypto from 'crypto'
+
+const cspHashOf = (text) => {
+  const hash = crypto.createHash('sha256')
+  hash.update(text)
+  return `'sha256-${hash.digest('base64')}'`
+}
 
 export default class Document extends NextDocument {
+
   render() {
+    let csp = `default-src 'self'; img-src https://*.jawg.io 'self' data:; style-src 'self' 'unsafe-inline'; script-src 'self' ${cspHashOf(
+      NextScript.getInlineScriptSource(this.props)
+    )}`
+    if (process.env.NODE_ENV !== 'production') {
+      csp = `default-src 'self'; img-src https://*.jawg.io 'self' data:; style-src 'self' 'unsafe-inline'; script-src 'unsafe-inline' 'unsafe-eval' 'self' `
+    }
+
+// ${cspHashOf(NextScript.getInlineScriptSource(this.props))}
     return (
       <Html lang='en'>
-        <Head />
+        <Head>
+          <meta httpEquiv="Content-Security-Policy" content={csp} />
+        </Head>
         <body>
           <ColorModeScript initialColorMode={theme.initialColorMode} />
           <Main />
