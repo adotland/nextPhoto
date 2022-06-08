9d316d894b315722445e98a028c453427189768c
adotland <1982042+adotland@users.noreply.github.com>
2022-06-08 01:01:28 -0700

    sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,34 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
   siteUrl: process.env.SITE_URL || 'https://example.com',
-  generateRobotsTxt: true, // (optional)
-  // ...other options
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  changefreq: 'weekly',
+  priority: 0.7,
+  autoLastmod: true,
+  transform: async (config, path) => {
+    if (customLimitedField(path)) {
+      return {
+        loc: path,
+        changefreq: 'always',
+        priority: config.priority,
+        lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+        alternateRefs: config.alternateRefs ?? [],
+      }
+    }
+    // Use default transformation for all other cases
+    return {
+      loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    }
+  },
+}
+
+function customLimitedField(path) {
+  return path.indexOf('/featured') !== -1
 }
 
 module.exports = config
