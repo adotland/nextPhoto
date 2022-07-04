5cb945b8183807ffcbae79773686986c628a277d
adotland <1982042+adotland@users.noreply.github.com>
2022-07-04 15:41:06 -0700

    build fuse once

--- pages/api/park/index.js
+++ pages/api/park/index.js
@@ -2,14 +2,16 @@ import Fuse from 'fuse.js';
 
 import { dataList } from './park_search_data'
 
+const options = {
+  includeScore: false,
+  keys: ['parkName']
+}
+
+const fuse = new Fuse(dataList, options)
+
 export default async function handler(req, res) {
   let { query } = (req.query)
   query = query.substring(0, 10).replaceAll(/[^a-z\s]/ig, '')?.toLowerCase();
-  const options = {
-    includeScore: false,
-    keys: ['parkName']
-  }
-  const fuse = new Fuse(dataList, options)
   const retval = fuse.search(query)
   return res.status(200).json({ props: { searchDataList: retval.slice(0, 5) } });
 }
