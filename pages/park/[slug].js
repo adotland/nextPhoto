54c43aa6b034f8aed6648b59335e4f5cbc585e2c
adotland <1982042+adotland@users.noreply.github.com>
2022-07-16 15:37:33 -0700

    video format and style

--- pages/park/[slug].js
+++ pages/park/[slug].js
@@ -8,53 +8,106 @@ import { byWeight, shimmer, shuffle, toBase64 } from "../../utils/helpers";
 import ImageVersionLink from "../../components/ImageVersionLink";
 
 export async function getStaticPaths() {
-  const collectionList = await ff.readJson('./data', 'enabled_collections.json');
-  const dataList = (await Promise.all(collectionList.map(async collection => await ff.readJson(ff.path(`./data/${collection}_data.json`))))).flat();
-  const displayable = dataList.filter(data => data.filters.live).map(data => {
-    // const displayable = dataList.map(data => {
-    return {
-      params: { slug: data.slug }
-    }
-  })
+  const collectionList = await ff.readJson(
+    "./data",
+    "enabled_collections.json"
+  );
+  const dataList = (
+    await Promise.all(
+      collectionList.map(
+        async (collection) =>
+          await ff.readJson(ff.path(`./data/${collection}_data.json`))
+      )
+    )
+  ).flat();
+  const displayable = dataList
+    .filter((data) => data.filters.live)
+    .map((data) => {
+      // const displayable = dataList.map(data => {
+      return {
+        params: { slug: data.slug },
+      };
+    });
   return {
     paths: displayable,
     fallback: false,
-  }
+  };
 }
 
 export async function getStaticProps({ params: { slug } }) {
-  const collectionList = await ff.readJson('./data', 'enabled_collections.json');
-  const dataList = (await Promise.all(collectionList.map(async collection => await ff.readJson(ff.path(`./data/${collection}_data.json`))))).flat();
-  const currentData = dataList.filter(d => d.slug == slug).pop();
-  let related = dataList
-    .filter(d => (d.filters.matchColor === currentData.filters.matchColor && d.slug !== currentData.slug && d.filters.live && d.ext === 'jpg' && ((d.height < d.width) && (d.filters.weight > 1))))
+  const collectionList = await ff.readJson(
+    "./data",
+    "enabled_collections.json"
+  );
+  const dataList = (
+    await Promise.all(
+      collectionList.map(
+        async (collection) =>
+          await ff.readJson(ff.path(`./data/${collection}_data.json`))
+      )
+    )
+  ).flat();
+  const currentData = dataList.filter((d) => d.slug == slug).pop();
+  let related = dataList.filter(
+    (d) =>
+      d.filters.matchColor === currentData.filters.matchColor &&
+      d.slug !== currentData.slug &&
+      d.filters.live &&
+      d.ext === "jpg" &&
+      d.height < d.width &&
+      d.filters.weight > 1
+  );
   related = shuffle(related.sort(byWeight)).slice(0, 3);
   return {
     props: {
       currentData,
-      related
-    }
+      related,
+    },
   };
 }
-
 
 export default function ParkSlug({ currentData, related }) {
   return (
     <>
       <SEO data={currentData} />
-      <Flex m={[0, 0, 0, 7]} justifyContent={'space-evenly'} flexDir={['column', 'column', 'column', 'row']} mt={[4, 4, 16, 4]}>
-        <Box flex={1}
+      <Flex
+        m={[0, 0, 0, 7]}
+        justifyContent={"space-evenly"}
+        flexDir={["column", "column", "column", "row"]}
+        mt={[4, 4, 16, 4]}
+      >
+        <Box
+          flex={1}
           minW={currentData.width > currentData.height ? "60%" : "40%"}
-          maxW={currentData.width > currentData.height ? ["100%", "100%", "100%", "65%"] : ["100%", "100%", "100%", "40%"]}
+          maxW={
+            currentData.width > currentData.height
+              ? ["100%", "100%", "100%", "65%"]
+              : ["100%", "100%", "100%", "40%"]
+          }
         >
-          {currentData.ext === 'webp' ?
-            <Box boxShadow={'lg'} width={currentData.width} mx='auto'>
-              <video width={currentData.width}
-                height={currentData.height}
-                src={`https://${process.env.NEXT_PUBLIC_IMG_HOST_DOMAIN}/video/t_${currentData.imageName.replace('webp','webm')}`}
+          {currentData.ext === "webp" ? (
+            <Box boxShadow={"lg"} maxW={currentData.width} minW={320} mx="auto">
+              <video
+                // width={currentData.width}
+                //   height={currentData.height}
+                // maxW
                 autoPlay={true}
                 loop={true}
-/>
+                style={{ width: "100%", maxHeight: "100%;" }}
+              >
+                <source
+                  src={`https://${
+                    process.env.NEXT_PUBLIC_IMG_HOST_DOMAIN
+                  }/video/${currentData.imageName.replace("webp", "mp4")}`}
+                  type="video/mp4"
+                />
+                <source
+                  src={`https://${
+                    process.env.NEXT_PUBLIC_IMG_HOST_DOMAIN
+                  }/video/${currentData.imageName.replace("webp", "webm")}`}
+                  type='video/webm; codecs="vp9, vorbis"'
+                />
+              </video>
               {/* <Image
                 key={currentData.id}
                 src={`https://${process.env.NEXT_PUBLIC_IMG_HOST_DOMAIN}/video/${currentData.imageName.replace('webp','webm')}`}
@@ -67,8 +120,8 @@ export default function ParkSlug({ currentData, related }) {
                 priority
               /> */}
             </Box>
-            :
-            <Box boxShadow={'lg'}>
+          ) : (
+            <Box boxShadow={"lg"}>
               {/* <Box style={{boxShadow: `0px 0px 3px 0px ${currentData.filters.domColor}`}}> */}
               <Image
                 key={currentData.id}
@@ -79,15 +132,17 @@ export default function ParkSlug({ currentData, related }) {
                 height={currentData.height}
                 // sizes="50vw"
                 placeholder="blur"
-                blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(currentData.width, currentData.height))}`}
+                blurDataURL={`data:image/svg+xml;base64,${toBase64(
+                  shimmer(currentData.width, currentData.height)
+                )}`}
                 priority
               />
             </Box>
-          }
+          )}
         </Box>
         <Details
           data={currentData}
-          textAlign={['center', 'center', 'center', 'left']}
+          textAlign={["center", "center", "center", "left"]}
           lineHeight={10}
           mt={["2em", "1.5em", "1em", 0]}
           ml={[0, 0, 0, 10]}
@@ -96,9 +151,13 @@ export default function ParkSlug({ currentData, related }) {
           width={["100%", "100%", "100%", "40rem"]}
         />
       </Flex>
-      {currentData.ext === 'jpg' && currentData.hasAnim && <ImageVersionLink slug={currentData.slug} type={'animated'} />}
-      {currentData.ext === 'webp' && currentData.hasAnim && <ImageVersionLink slug={currentData.slug} type={'still'} />}
+      {currentData.ext === "jpg" && currentData.hasAnim && (
+        <ImageVersionLink slug={currentData.slug} type={"animated"} />
+      )}
+      {currentData.ext === "webp" && currentData.hasAnim && (
+        <ImageVersionLink slug={currentData.slug} type={"still"} />
+      )}
       <RelatedImages dataList={related} />
     </>
-  )
+  );
 }
