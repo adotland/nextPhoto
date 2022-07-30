a4ba3fa5f96a988515f2b4ee9a82ff09443c3d78
adotland <1982042+adotland@users.noreply.github.com>
2022-07-29 21:33:56 -0700

    add sentry, update next ...!

--- config/config-prod.js
+++ config/config-prod.js
@@ -5,7 +5,7 @@ default-src 'self';
 img-src 'self' https://*.jawg.io data:;
 style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
 script-src 'self' 'unsafe-inline';
-connect-src 'self' https://vitals.vercel-insights.com https://umami-seven-weld.vercel.app;
+connect-src 'self' https://vitals.vercel-insights.com https://umami-seven-weld.vercel.app https://o1339884.ingest.sentry.io;
 font-src 'self' data: https://fonts.gstatic.com;
 media-src https://theparkandthebike.s3.us-west-2.amazonaws.com;
 `
--- pages/api/park/index.js
+++ pages/api/park/index.js
@@ -1,3 +1,4 @@
+import { withSentry } from "@sentry/nextjs";
 import path from "path";
 import * as fs from "fs";
 const dataList = JSON.parse(
@@ -14,12 +15,14 @@ const options = {
 };
 const fuse = new Fuse(dataList, options);
 
-export default async function handler(req, res) {
+async function handler(req, res) {
   let { query } = req.query;
   query = query
     .substring(0, 10)
     .replaceAll(/[^a-z\s]/gi, "")
     ?.toLowerCase();
-  const retval = fuse.search(query);
-  res.status(200).json({ props: { searchDataList: retval.slice(0, 5) } });
+  const result = fuse.search(query);
+  res.status(200).json({ props: { searchDataList: result.slice(0, 5) } });
 }
+
+export default withSentry(handler);
