29ff0136fa47a0d01dde673cd12585e02d849b6b
A.Land <1982042+adotland@users.noreply.github.com>
2023-01-24 00:50:47 -0800

    home updates

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,56 @@ import Link from "next/link";
 import SEO from "../components/SEO/general";
 import styles from "../components/Home.module.css";
 import { GoBeaker } from "react-icons/go";
-import { FaMapMarkerAlt, FaDice } from "react-icons/fa";
+import { FaTree } from "react-icons/fa";
 import Image from "next/image";
+import { BsMap, BsPinMapFill } from "react-icons/bs";
+
+function MainHomeTextBox({ children }) {
+  return (
+    <Flex
+    flexDir={'column'}
+      color={useColorModeValue("#111", "#eee")}
+      fontSize={["xl", "xl", "2xl"]}
+      w={["303px", "333px"]}
+      h={['initial', 'initial', 'initial', '90%']}
+      maxH={'420px'}
+      mr={["initial", "initial", "initial", "5em"]}
+      alignItems={'flex-start'}
+      zIndex={1}
+      pos={"relative"}
+      // letterSpacing={"0.2em"}
+      my={4}
+      p={4}
+      align={"center"}
+      // boxShadow={"xl"}
+      _after={{
+        backgroundColor: useColorModeValue("rgba(255,255,255,0.5)", "rgba(255,255,255,0.2)"),
+        opacity: "80%",
+        backdropFilter: "saturate(200%) blur(10px)",
+        position: "absolute",
+        content: "''",
+        zIndex: -1,
+        h: "100%",
+        w: "100%",
+        right: 0,
+        top: 0,
+        borderRadius: 10
+      }}
+    >
+      {children}
+      <Box className={styles.profileImage}>
+        <Image
+          src={'/profile.jpg'}
+          alt={"profile image"}
+          layout={"fixed"}
+          width={'222px'}
+          height={'222px'}
+          objectFit={"cover"}
+        />
+      </Box>
+    </Flex>
+  );
+}
 
 function HomeTextBox({ children }) {
   return (
@@ -59,17 +107,21 @@ export async function getStaticProps() {
     props: {
       videoLink: `https://${process.env.NEXT_PUBLIC_IMG_HOST_DOMAIN}/video/seattle_video_3mBR_loop_1.mp4`,
       bgLink: `https://${process.env.NEXT_PUBLIC_IMG_HOST_DOMAIN}/general/background_map.jpg`,
+      mainImgLink: `https://${process.env.NEXT_PUBLIC_IMG_HOST_DOMAIN}/310_1301_Discovery-Park.jpg`,
     },
   };
 }
 
-export default function IndexPage({ videoLink, bgLink }) {
+export default function IndexPage({ videoLink, bgLink, mainImgLink }) {
+  const gradColor = useColorModeValue('#33d0ff', '#33d0ff')
+  const bgColor = useColorModeValue('white', 'black')
   return (
     <Box mt={[4, 4, 14, 4]}>
       <SEO pageTitle={"Home"} />
       <Box
-      // className={`${styles.videoBox} ${useColorModeValue("", styles.videoBoxDark)}`}
-      className={`${styles.videoBox}`}
+        // className={`${styles.videoBox} ${useColorModeValue("", styles.videoBoxDark)}`}
+        className={`${styles.videoBox}`}
+        _after={{ backgroundImage: `linear-gradient(0deg, ${bgColor}, ${gradColor})` }}
       >
         <Image
           src={bgLink}
@@ -88,9 +140,9 @@ export default function IndexPage({ videoLink, bgLink }) {
         mx={"auto"}
         pt={["1px", "1px", "24px"]}
       >
-        <Flex wrap={["wrap", "wrap", "nowrap"]} justify={["space-between"]}>
+        <Flex wrap={["wrap", "wrap", "nowrap"]} justify={["space-between"]} >
           <Box w={["100%", "100%", "100%", "40%"]} mr={[0, 0, "24px"]}>
-            <HomeTextBox>
+            <MainHomeTextBox img={mainImgLink}>
               <div>
                 Welcome to{" "}
                 <span style={{ fontWeight: "bold" }}>TheParkAndTheBike</span>,
@@ -99,15 +151,22 @@ export default function IndexPage({ videoLink, bgLink }) {
                 <br />
                 the Greater Seattle Area
               </div>
-            </HomeTextBox>
+            </MainHomeTextBox>
           </Box>
           <Flex wrap={"wrap"}>
+            <HomeLinkBox link={"/routes"}>
+              <div>
+                Discover parks on <br />
+                <span style={{ fontWeight: "bold" }}>Routes</span>
+              </div>
+              <BsPinMapFill size={"1.4em"} />
+            </HomeLinkBox>
             <HomeLinkBox link={"/map"}>
               <div>
                 Find parks on the <br />
                 interactive <span style={{ fontWeight: "bold" }}>Map</span>
               </div>
-              <FaMapMarkerAlt size={"1.4em"} />
+              <BsMap size={"1.4em"} />
             </HomeLinkBox>
             <HomeLinkBox link={"/map/seattle-parks-and-health"}>
               <div>
@@ -123,7 +182,7 @@ export default function IndexPage({ videoLink, bgLink }) {
                 <br /> in the random <br />
                 <span style={{ fontWeight: "bold" }}>Featured gallery</span>
               </div>
-              <FaDice size={"2em"} />
+              <FaTree size={"2em"} />
             </HomeLinkBox>
             <HomeTextBox>
               <div>
