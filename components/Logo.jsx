79179251568a8f8ab2d8d86ff13581d83b1d1341
adotland <adotland@gmail.com>
2025-02-27 19:11:21 -0800

    logo spacing, add prettier

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,28 +1,28 @@
-import { Box, Text, useColorModeValue } from "@chakra-ui/react";
-import Link from "next/link";
+import { Box, Text, useColorModeValue } from '@chakra-ui/react';
+import Link from 'next/link';
 
 export default function Logo(props) {
   return (
-    <Box {...props} mb={[0, 0, 5, 0]} alignSelf={'center'}>
-      <Link href="/">
+    <Box {...props} mb={[0, 0, 5, 0]} ml={[0, 0, 0, 5]} alignSelf={'center'}>
+      <Link href='/'>
         <a>
           <Text
             fontFamily={'Open sans'}
-            fontSize="md"
-            fontWeight="bold"
+            fontSize='md'
+            fontWeight='bold'
             minW={180}
-            color={useColorModeValue("brand.700", "brand.200")}
+            color={useColorModeValue('brand.700', 'brand.200')}
             // color={useColorModeValue("white", "black")}
-            backgroundColor={useColorModeValue("white", "#191a1a")}
+            backgroundColor={useColorModeValue('white', '#191a1a')}
             p={1}
             transform={'skew(-21deg)'}
-          // bgGradient={useColorModeValue("linear(to-l, green.300 ,#000)", "linear(to-l, green.300 ,#fff)")}
-          // bgClip="text"
+            // bgGradient={useColorModeValue("linear(to-l, green.300 ,#000)", "linear(to-l, green.300 ,#fff)")}
+            // bgClip="text"
           >
             Bike^Park
           </Text>
         </a>
       </Link>
     </Box>
-  )
+  );
 }
--- pages/api/routes/index.js
+++ pages/api/routes/index.js
@@ -1,26 +1,26 @@
-import { withSentry } from "@sentry/nextjs";
-import path from "path";
-import * as fs from "fs";
+import { withSentry } from '@sentry/nextjs';
+import path from 'path';
+import * as fs from 'fs';
 
 const readFile = (filePath, fileName) => {
   const fullPath = path.join(process.cwd(), filePath, fileName);
-  return fs.readFileSync(fullPath, "utf8");
-}
+  return fs.readFileSync(fullPath, 'utf8');
+};
 
 const readJson = (filePath, fileName) => {
-  const dataStr = readFile(filePath, fileName)
+  const dataStr = readFile(filePath, fileName);
   return JSON.parse(dataStr);
-}
+};
 
 const santize = (input) => {
   return input
     .substring(0, 100)
-    .replaceAll(/[^a-z-\s]/gi, "")
+    .replaceAll(/[^a-z-\s]/gi, '')
     ?.toLowerCase();
-}
+};
 
-const routeDataList = readJson("./data/", "routes.json");
-const collectionList = readJson("./data/", "enabled_collections.json");
+const routeDataList = readJson('./data/', 'routes.json');
+const collectionList = readJson('./data/', 'enabled_collections.json');
 
 let parksDataList = [];
 collectionList.forEach((collection) =>
@@ -28,17 +28,20 @@ collectionList.forEach((collection) =>
 );
 parksDataList = parksDataList.flat();
 
-
 async function handler(req, res) {
-  let { slug } = req.query
+  let { slug } = req.query;
   slug = santize(slug);
-  const routeData = routeDataList.find(d => d.slug === slug);
+  const routeData = routeDataList.find((d) => d.slug === slug);
   if (routeData) {
-    const parkSlugList = routeData.parkList.map(p => p.slug);
-    const routeParkDataList = parksDataList.filter(p => parkSlugList.includes(p.slug));
-    res.status(200).send(JSON.stringify({ ...routeData, parkDataList: routeParkDataList }));
+    const parkSlugList = routeData.parkList.map((p) => p.slug);
+    const routeParkDataList = parksDataList.filter((p) =>
+      parkSlugList.includes(p.slug)
+    );
+    res
+      .status(200)
+      .send(JSON.stringify({ ...routeData, parkDataList: routeParkDataList }));
   } else {
-    res.status(404).send({ error: 'failed to fetch data' })
+    res.status(404).send({ error: 'failed to fetch data' });
   }
 }
 
